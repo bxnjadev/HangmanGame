26e1ba864d66256cac16013f48a0c53b3c878034
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-01-19 11:50:50 -0300

    added login server logic

--- server/main_server.py
+++ server/main_server.py
@@ -2,10 +2,11 @@ from helper import encoder
 from helper.reader_file import ReaderFile
 import socket
 
-from server import client_message_result, client_handler
+from server import client_message_result, client_handler, message_response
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 LIMIT_BYTES = 1024
+
 
 def __init__():
 
@@ -26,6 +27,26 @@ def listen():
     client, address = socket_server.accept()
     message_in_bytes = client.recv(LIMIT_BYTES)
     message = encoder.decode(message_in_bytes)
+
+    content_message_response = message_response.parse(message)
+
+    if content_message_response.get_type() == "new_authentication":
+        client.send(
+            "login:_"
+        )
+        return
+
+    if content_message_response.get_type() == "login":
+        client.send(
+            "message:login-successful"
+        )
+
+        client_handler.register_client(
+            content_message_response.get_message(),
+            client
+        )
+        print("User registered: ", content_message_response.get_message())
+        return
 
     action_response = client_message_result.get_response(message)
 
