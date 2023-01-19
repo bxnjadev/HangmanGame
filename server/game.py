83cafe3cb1b07d819fd16627711ea55885130bb2
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-01-19 12:51:06 -0300

    added base game

---
+++ server/game.py
@@ -0,0 +1,26 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'q',' r', 's', 't', 'u', 'w', 'x', 'y', 'z']
+
+
+class HangmanGame:
+
+    def __init__(self):
+        self.position_turn = 0
+        self.started = False
+        self.letters_available = []
+        self.word = ""
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def next_turn(self):
+        pass
+
+    def select_word(self):
+        pass
+
+    def play(self, client_wrapper, letter):
+        pass
